a968b4d9f337e49bc69e5c761d7282c2d70cfbcd
huoxudong125 <423huo@163.com>
2016-03-11 19:23:22 +0800

    Init Solution

---
+++ HQF.Tools.Renamer/FileNameFixer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace HQF.Tools.Renamer
+{
+    public class FileNameFixer
+    {
+        public FileNameFixer()
+        {
+            StringToRemove = "/";
+            StringReplacement = "_";
+        }
+
+        public void FixAll(string rootDirectory)
+        {
+            var directories = ShowAllFoldersUnder(rootDirectory, 2);
+            foreach (var directory in directories)
+            {
+                IEnumerable<string> files = Directory.EnumerateFiles(directory);
+                foreach (string file in files)
+                {
+                    try
+                    {
+                        FileInfo info = new FileInfo(file);
+                        if (!info.IsReadOnly && !info.Attributes.HasFlag(FileAttributes.System))
+                        {
+                            string destFileName = GetNewFile(file);
+                            info.MoveTo(destFileName);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.Write(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private string GetNewFile(string file)
+        {
+            string nameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+            if (nameWithoutExtension != null && nameWithoutExtension.Length > 1)
+            {
+                return Path.Combine(Path.GetDirectoryName(file),
+                    file.Replace(StringToRemove, StringReplacement));// + Path.GetExtension(file));
+            }
+            return file;
+        }
+
+        private static List<string> ShowAllFoldersUnder(string path, int indent)
+        {
+            var result = new List<string>();
+            try
+            {
+                if ((File.GetAttributes(path) & FileAttributes.ReparsePoint)
+                    != FileAttributes.ReparsePoint)
+                {
+                    foreach (string folder in Directory.GetDirectories(path))
+                    {
+                        Console.WriteLine(
+                            "{0}{1}", new string(' ', indent), Path.GetFileName(folder));
+
+                        result.Add(folder);
+
+                        result.AddRange(ShowAllFoldersUnder(folder, indent + 2));
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return result;
+        }
+
+        public string StringToRemove { get; set; }
+
+        public string StringReplacement { get; set; }
+    }
+}
---
+++ HQF.Tools.Renamer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HQF.Tools.Renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fixer = new FileNameFixer();
+            if (args.Length > 0)
+            {
+                fixer.FixAll(args[0]);
+            }
+        }
+
+
+      
+    }
+}
